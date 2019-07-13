38bc7b66b58945256d5e43f3fd23f3e14600aefe
Yaroslav Georgitsa <yaroslav.georgitsa@gmail.com>
2019-07-13 13:54:31 +0300

    added Roles enum

---
+++ app/Domain/User/Enums/RoleEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Domain\User\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self admin()
+ * @method static self subscriber()
+ * @method static self member()
+ */
+class RoleEnum extends Enum
+{
+
+}
